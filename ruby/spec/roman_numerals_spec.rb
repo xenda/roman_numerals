72660233e4b7c383546f53d686a155e6f31d7185
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-21 01:01:18 -0500

    Convert3toIII test

--- ruby/spec/roman_numerals_spec.rb
+++ ruby/spec/roman_numerals_spec.rb
@@ -2,13 +2,20 @@ require 'roman_numerals'
 
 describe RomanNumerals do
 
-  it "converts 1 to I" do
-    RomanNumerals.to_roman(1).should == "I"
-  end
+  context "Converts 1..3 to romans" do
 
-  it "converts 2 to II" do
-    RomanNumerals.to_roman(2).should == "II"
-  end
+    it "converts 1 to I" do
+      RomanNumerals.to_roman(1).should == "I"
+    end
 
+    it "converts 2 to II" do
+      RomanNumerals.to_roman(2).should == "II"
+    end
+
+    it "converts 3 to III" do
+      RomanNumerals.to_roman(3).should == "III"
+    end
+
+  end
 
 end
