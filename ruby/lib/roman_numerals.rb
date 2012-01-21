1cbecd1260ffba78acd11456a3ca0b2b1d66ae99
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-21 18:05:18 -0500

    Refactor .==(0) to .zero?

--- ruby/lib/roman_numerals.rb
+++ ruby/lib/roman_numerals.rb
@@ -14,7 +14,7 @@ module RomanNumerals
     ROMANS.each do |key, value|
       count, numeral = numeral.divmod(value)
       roman << (key * count)
-      break if numeral == 0
+      break if numeral.zero?
     end
 
     roman
